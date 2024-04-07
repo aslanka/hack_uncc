9eb3d6dac3ce4363e4610e879f29463184f924bb
aslanka <ayushlanka106@gmail.com>
2024-04-07 00:52:21 -0400

    update to challege_id for images

--- backend/Routes/media_upload.py
+++ backend/Routes/media_upload.py
@@ -7,56 +7,108 @@ import json
 from utils import get_user
 from werkzeug.utils import secure_filename
 from gridfs import GridFS
+import base64
 
 fs = GridFS(db)
 @app.route('/upload/image', methods=['POST'])
 def upload_image():
-    if 'image' in request.files:
-        image = request.files['image']
-        if image.filename != '':
-            filename = secure_filename(image.filename)
-            image_id = fs.put(image, filename=filename)
-            # image_id = fs.put(image.stream, filename=secure_filename(image.filename))
-            return 'Image uploaded successfully: {}'.format(image_id)
-        else:
-            return 'Invalid image file'
-    else:
-        return 'No image part in the request'
-
-@app.route('/upload/video', methods=['POST'])
-def upload_video():
-    if 'video' in request.files:
-        video = request.files['video']
-        if video.filename != '':
-            filename = secure_filename(video.filename)
-            video_id = fs.put(video, filename=filename)
-            return 'Video uploaded successfully: {}'.format(str(video_id))
-        else:
-            return 'Invalid video file'
-    else:
-        return 'No video part in the request'
-
-@app.route('/image/<image_id>', methods=['GET'])
-def get_image(image_id):
     try:
-        image_object = fs.get(ObjectId(image_id))
-        if image_object:
-            # Serve the image file
-            return send_file(image_object, mimetype='image/jpeg')
+        # Get challenge_id from the request body or URL parameters
+        challenge_id = request.form.get('challenge_id')
+        if not challenge_id:
+            return jsonify({"error": "Challenge ID is required"}), 400
+
+        if 'image' in request.files:
+            image = request.files['image']
+            if image.filename != '':
+                # Save image to GridFS
+                filename = secure_filename(image.filename)
+                image_id = fs.put(image, filename=filename)
+                # Associate image with challenge by updating challenge document in MongoDB
+                db.Challenges.update_one({'_id': ObjectId(challenge_id)}, {'$push': {'images': str(image_id)}})
+                return jsonify({"message": "Image uploaded successfully", "image_id": str(image_id)})
+            else:
+                return jsonify({"error": "Invalid image file"}), 400
         else:
-            return jsonify({"error": "Image not found"}), 404
+            return jsonify({"error": "No image part in the request"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/video/<video_id>', methods=['GET'])
-def get_video(video_id):
+
+@app.route('/upload/video', methods=['POST'])
+def upload_video():
     try:
-        fs = GridFS(db)
-        video_object = fs.get(ObjectId(video_id))
-        if video_object:
-            # Serve the video file
-            return send_file(video_object, mimetype='video/mp4')
+        # Get challenge_id from the request body or URL parameters
+        challenge_id = request.form.get('challenge_id')
+        if not challenge_id:
+            return jsonify({"error": "Challenge ID is required"}), 400
+
+        if 'video' in request.files:
+            video = request.files['video']
+            if video.filename != '':
+                # Save video to GridFS
+                filename = secure_filename(video.filename)
+                video_id = fs.put(video, filename=filename)
+                # Associate video with challenge by updating challenge document in MongoDB
+                db.Challenges.update_one({'_id': ObjectId(challenge_id)}, {'$push': {'videos': str(video_id)}})
+                return jsonify({"message": "Video uploaded successfully", "video_id": str(video_id)})
+            else:
+                return jsonify({"error": "Invalid video file"}), 400
         else:
-            return jsonify({"error": "Video not found"}), 404
+            return jsonify({"error": "No video part in the request"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/images/<challenge_id>', methods=['GET'])
+def get_images_by_challenge_id(challenge_id):
+    try:
+        # Retrieve list of image IDs associated with the given challenge ID
+        challenge = db.Challenges.find_one({'_id': ObjectId(challenge_id)})
+        if challenge:
+            images = challenge.get('images', [])
+            if images:
+                image_data = []
+                for image_id in images:
+                    image_object = fs.get(ObjectId(image_id))
+                    if image_object:
+                        # Return image file using send_file
+                        return send_file(image_object, mimetype='image/jpeg')
+                return jsonify({"error": "No images found for this challenge"}), 404
+            else:
+                return jsonify({"error": "No images found for this challenge"}), 404
+        else:
+            return jsonify({"error": "Challenge not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+# @app.route('/videos/<challenge_id>', methods=['GET'])
+# def get_videos_by_challenge_id(challenge_id):
+#     try:
+#         # Retrieve list of video IDs associated with the given challenge ID
+#         challenge = db.Challenges.find_one({'_id': ObjectId(challenge_id)})
+#         if challenge:
+#             videos = challenge.get('videos', [])
+#             return jsonify({"videos": videos})
+#         else:
+#             return jsonify({"error": "Challenge not found"}), 404
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
+
+@app.route('/videos/<challenge_id>', methods=['GET'])
+def get_videos_by_challenge_id(challenge_id):
+    try:
+        # Retrieve list of video IDs associated with the given challenge ID
+        challenge = db.Challenges.find_one({'_id': ObjectId(challenge_id)})
+        if challenge:
+            videos = challenge.get('videos', [])
+            if videos:
+                # Retrieve and send the first video file associated with the challenge
+                video_id = videos[0]  # Assuming you want to send the first video
+                video_object = fs.get(ObjectId(video_id))
+                return send_file(video_object, mimetype='video/mp4')
+            else:
+                return jsonify({"error": "No videos found for this challenge"}), 404
+        else:
+            return jsonify({"error": "Challenge not found"}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
